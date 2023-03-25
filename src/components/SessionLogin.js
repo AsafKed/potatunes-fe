1ec46e65a23e7800ed6889fadbba983bfde48377
AsafKed <asafkedem97@gmail.com>
2023-03-25 19:54:56 +0100

    Enable fe to have a callback with the be

--- src/components/SessionLogin.js
+++ src/components/SessionLogin.js
@@ -2,15 +2,27 @@ import React, { useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import favicon from '../assets/favicon.ico';
 
+
+// If local host, use localhost:3000 as the redirect URI. Otherwise, use the production URL.
+const domain = (window.location.href.includes('localhost') || 
+                window.location.href.includes('127.0.0.1')) ?
+    'http://127.0.0.1:5000/callback' : // redirects to the backend to process the addition of the user
+    'https://potatunes.com/callback';
+
+// Spotify authorization URL
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${domain}&scope=${process.env.REACT_APP_SCOPE}`;
+
 function SessionLogin() {
   const [listeningSessionId, setListeningSessionId] = useState(`${Math.random().toString(36).substring(2, 15)}`);
   const [spotifyLink, setSpotifyLink] = useState(`https://potatunes.com/${listeningSessionId}`);
+  const [spotifyAuthLink, setSpotifyAuthLink] = useState(`${AUTH_URL}&state=${listeningSessionId}`);
 
   // TODO: store the listeningSessionId as a cookie so that it persists across page refreshes
   // This function generates a new listening session ID and updates the QR code
   function handleNewSession() {
     setListeningSessionId(Math.random().toString(36).substring(2, 15));
     setSpotifyLink(`https://potatunes.com/${listeningSessionId}`);
+    setSpotifyAuthLink(`${AUTH_URL}&state=${listeningSessionId}`)
   }
 
   // Define custom styles for the QR code
@@ -39,14 +51,16 @@ function SessionLogin() {
     justifyContent: 'center',
   };
 
+  console.log(AUTH_URL);
+
   return (
     <div style={pageStyle}>
-      <QRCodeSVG value={spotifyLink} style={qrCodeStyle} key={listeningSessionId} />
+      <QRCodeSVG value={spotifyAuthLink} style={qrCodeStyle} key={listeningSessionId} />
       <p>Current session id: {listeningSessionId}</p>
       <p>Scan the QR code to join the listening session. Or, click the button below to generate a new listening session.</p>
       <div>
         <button style={buttonStyle} onClick={handleNewSession}>New listening session</button>
-        <a style={buttonStyle} href="http://localhost:5000">Add user</a>
+        <a style={buttonStyle} href={spotifyAuthLink}>Add user</a>
       </div>
 
     </div>
